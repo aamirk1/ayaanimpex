6f3f3b82ac70fcc9e1552812f831069010dff44f
unknown <aamir.k7199@gmail.com>
2024-10-17 18:25:28 +0530

    update

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php $base_url = 'http://localhost/ayaanimpex/'; ?>
+
 <footer class="site-footer footer-dark" style="display: block; height: 528.688px;">
 
     <!-- FOOTER BLOCKES START -->
@@ -146,12 +146,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
-
-
-
-
-
-
 
 
 </body>
